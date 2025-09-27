aa107646066bf594e2ca0d072d455166e690154c
Patrick Sasso <patricksassorj@gmail.com>
2025-09-27 18:05:38 -0300

    CI para publicaçao no gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 
+const basePath = process.env.BASE_PATH ?? '/';
+
 export default defineConfig({
+  base: basePath,
   server: {
     port: 5173,
     open: true
